f4a51c1d5a083acb0a797eae10ad292ea4641444
Maha Alharbi <m.a.alharbi995@gmail.com>
2021-12-08 14:06:55 +0300

    userRoute , verifyToken

--- blush-app/backend/routes/user.js
+++ blush-app/backend/routes/user.js
@@ -2,9 +2,9 @@ const router = require('express').Router();
 const User = require("../models/User");
 const CryptoJS = require("crypto-js");
 const jwt = require("jsonwebtoken");
-const {verifyToken, verifyTokenAndAuthorization} = require("./verifyToken");
+const {verifyToken, verifyTokenAndAuthorization,verifyTokenAndAdmin} = require("./verifyToken");
 
-//==== Ubdating ====// 
+//==== Ubdating ====// << second argument "midlware" to verify WebToken and get Authentecation
 router.put("/:id",verifyTokenAndAuthorization, async (req,res) => {
 // check if update user password will encrypt agin
 if (req.body.password ){
@@ -20,7 +20,35 @@ try {
 } catch (error) {
     res.status(500).json(error);
 }
-}) //second argument "midlware" to verify WebToken
+}); 
+//==== Deleting ====// 
+router.delete("/:id",verifyTokenAndAuthorization, async (req,res,) =>{
+    try {
+        await User.findByIdAndDelete(req.params.id);
+        res.status(200).json("User has been deleted ...")
+    } catch (error) {
+        res.status(500).json(error)
+    }
+});
+//==== GET USER BY ID ====// << just Admin can  get user
+router.get("/find/:id",verifyTokenAndAdmin, async (req,res,) =>{
+    try {
+        const user = await User.findById(req.params.id);
+        res.status(200).json(user)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+});
+//==== GET ALL USER ====// << just Admin can  get user
+router.get("/",verifyTokenAndAdmin, async (req,res,) =>{
+    const query = req.query.new //new is a varible  if have any condition on the search  => localhost:8080/api/users?new=true
+    try {
+        const users = query? await User.find().sort({_id:-1}).limit(5) : await User.find();
+        res.status(200).json(users)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+});
 
 
 module.exports = router;
