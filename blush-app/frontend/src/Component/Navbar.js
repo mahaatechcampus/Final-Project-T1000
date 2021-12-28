e1d2c46912b02511d43dda5aed86fc2b60e03db7
Maha Alharbi <m.a.alharbi995@gmail.com>
2021-12-28 15:59:57 +0300

    edit on navbar bottom style

--- blush-app/frontend/src/Component/Navbar.js
+++ blush-app/frontend/src/Component/Navbar.js
@@ -14,6 +14,25 @@ const quantitywish =  useSelector(state => state.wishlist.quantity)
 const user = useSelector((state) => state.user.currentUser);
 const navigate = useNavigate();
 
+let arr = [true, false, false, false, false, false]
+    const [style, setStyle] = useState(arr);
+    const [dropDown, setDropDown] = useState(true);
+    const [text, setText] = useState("");
+
+    const selected = (props) => {
+        let newArr = [...arr];
+        for (let i = 0; i < newArr.length; i++) {
+            newArr[i] = false;
+        }
+        newArr[props] = true;
+        setStyle(newArr);
+    }
+
+    const setSelectedText = (txt) => {
+        setText(txt);
+        setDropDown(true);
+    }
+
 
     return (
         <>
@@ -83,14 +102,47 @@ const navigate = useNavigate();
 
 
         {/* bottom Navbar */}
-        <div className="bg-color1">
+        <div className="bg-color1 py-2">
             <div className="container flex h-10">
-                <div className="flex items-center justify-center flex-grow ">
-                    <div className="flex items-center space-x-6 capitalize">
-                        <Link to="/products/eye" className="text-color11 hover:text-color3 transition" >Eye</Link>
+                <div className="flex items-center justify-center flex-grow  ">
+                        
+
+
+                    <nav className="flex items-center space-x-6 capitalize ">
+
+                    {/* For medium and plus sized devices */}
+                    <ul className="hidden md:flex  flex-auto space-x-2">
+                        <Link to="/products/eye"><li onClick={()=>selected(0)} className={`${style[0] ? 'text-color11 bg-color2 font-medium text-base' : 'text-color11 border border-color1 font-medium text-base '}  focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-color2  cursor-pointer px-3 py-2.5 font-normal text-xs leading-3 shadow-md rounded`}>Eye</li></Link>
+                        <Link to="/products/face"><li onClick={()=>selected(1)} className={`${style[1] ? 'text-color11 bg-color2 font-medium text-base' : 'text-color11 border border-color1 font-medium text-base   '} focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 cursor-pointer px-3 py-2.5 font-normal text-xs leading-3 shadow-md rounded`}>Face</li></Link>
+                        <Link to="/products/lip"><li onClick={()=>selected(2)} className={`${style[2] ? 'text-color11 bg-color2 font-medium text-base' : 'text-color11 border border-color1 font-medium text-base '} focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 cursor-pointer px-3 py-2.5 font-normal text-xs leading-3 shadow-md rounded`}>Lip</li></Link>
+                    </ul>
+                </nav>
+                {/* for smaller devcies */}
+                <div className="block md:hidden w-full mt-5 text-color3 text-center ">
+                    <div onClick={()=>setDropDown(!dropDown)} className="cursor-pointer px-4 py-3 text-color11 font-medium text-base rounded flex justify-between items-center w-full">
+                        <div className="flex space-x-2">
+                            <span id="s1" className={`${text.length != 0 ? '' : 'hidden'} font-semibold text-sm leading-3`}>Selected: </span><p id="textClicked" className="font-normal text-sm leading-3 focus:outline-none  duration-100 cursor-pointer ">{text ? text : "Category"}</p>
+                        </div>
+                        <svg id="ArrowSVG" className={`${dropDown ? '' : 'rotate-180'} transform duration-100`} width={24} height={24} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M6 9L12 15L18 9" stroke="white" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+                        </svg>
+                    </div>
+                    <div className=" relative">
+                        <ul id="list" className={`${dropDown ? 'hidden' : 'block'} font-normal text-color3 text-base leading-4 absolute top-2  w-full rounded shadow-md`}>
+                        <Link to="/products/eye"><li onClick={()=>setSelectedText("Eye")} className="px-4 py-3 text-color11 bg-color1 border border-color1 focus:outline-none focus:bg-color2 hover:bg-color8 hover:text-color3 duration-100 cursor-pointer text-sm leading-3 font-normal">Eye</li></Link>
+                        <Link to="/products/face"><li onClick={()=>setSelectedText("Face")} className="px-4 py-3 text-color11 bg-color1 border border-color1 focus:outline-none focus:bg-color2 hover:bg-color8 hover:text-color3 duration-100 cursor-pointer text-sm leading-3 font-normal">Face</li></Link>
+                        <Link to="/products/lip"><li onClick={()=>setSelectedText("Lip")} className="px-4 py-3 text-color11 bg-color1 border border-color1 focus:outline-none focus:bg-color2 hover:bg-color8 hover:text-color3 duration-100 cursor-pointer text-sm  leading-3 font-normal">Lip</li></Link>
+                            
+                        </ul>
+                    </div>
+                </div>
+            
+
+
+                        {/* <Link to="/products/eye" className="text-color11 hover:text-color3 transition" >Eye</Link>
                         <Link to="/products/face" className="text-color11 hover:text-color3 transition" >Face</Link>
                         <Link to="/products/lip" className="text-color11 hover:text-color3 transition" >Lip</Link>
-                    </div>
+                    </div> */}
                 </div>
             </div>
         </div>
